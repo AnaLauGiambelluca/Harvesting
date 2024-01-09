03a2c687f5a84c7848031ef41bb02cd96124da27
AnaLauGiambelluca <148961897+AnaLauGiambelluca@users.noreply.github.com>
2024-01-09 11:21:07 +0100

    Update 3- Classification_and_Export.py
     This code allows export the result of using magnitude as variable to classify plot in different forest harvesting practices.

--- Auxiliary_Code/3- Classification_and_Export.py
+++ Auxiliary_Code/3- Classification_and_Export.py
@@ -1,5 +1,6 @@
-#Importe de librerias
+# This code allows export the result of using magnitude as variable to classify plot in different forest harvesting practices.
 
+#Import libraries
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
@@ -11,145 +12,72 @@ import rasterio.warp
 from osgeo import gdal
 from matplotlib import pyplot
 from rasterio.plot import show 
-
 import geopandas as gpd
-#Empezamos cargando librerías
 from osgeo import ogr, osr
-
 from sklearn.metrics import confusion_matrix
 from sklearn.metrics import ConfusionMatrixDisplay, classification_report,f1_score
 from sklearn.metrics import accuracy_score
-
 from sklearn.model_selection import train_test_split 
 
-os.chdir(r'C:\Users\giambelluca.144055\OneDrive\Doctorado\Datos_Laura')
+#First part: upload CSV and vectorial information
+direct='Path'
+os.chdir(direct)
+dC = pd.read_csv('Thinning_B7_B6.csv')
+dCon = pd.read_csv('Control_B7_B6.csv')
+dCH = pd.read_csv('CC_B7_B6.csv')
 
-dC = pd.read_csv('Claras_B7_B6_Prob_MAG.csv')
-dCon = pd.read_csv('Control_B7_B6_Prob_MAG.csv')
-dCH = pd.read_csv('Cortas_B7_B6_Prob_MAG.csv')
+direct_vect='Path'
+os.chdir(direct_vect)
+SHP_T=gpd.read_file('Thinning_plot.shp')
+SHP_Control=gpd.read_file('Control_plot.shp')
+SHP_CC=gpd.read_file('Clear_cutting_plot.shp')
 
-
-os.chdir(r'C:\Users\giambelluca.144055\OneDrive\Doctorado\Capas\Purgadas')
-SHP_Claras=gpd.read_file('Claras_10102023.shp')
-SHP_Control=gpd.read_file('Control_10102023.shp')
-SHP_Cortas=gpd.read_file('Cortas_10102023.shp')
-
-#Claras
-
-union_claras=pd.merge(dC, SHP_Claras, on='ID')
-
-union_claras['%2016']=(union_claras['P1_16']/(union_claras['N_pixel']))*100
-union_claras['%2017']=(union_claras['P1_17']/(union_claras['N_pixel']))*100
-union_claras['%2018']=(union_claras['P1_18']/(union_claras['N_pixel']))*100
-union_claras['%2019']=(+union_claras['P1_19']/(union_claras['N_pixel']))*100
-union_claras['%2020']=((union_claras['P1_20'])/(union_claras['N_pixel']))*100
-union_claras['%2021']=((union_claras['P1_21'])/(union_claras['N_pixel']))*100
-union_claras['%2022']=(union_claras['P1_22'])/(union_claras['N_pixel'])*100
-
-union_claras['Suma_2016']=union_claras['%2016']
-union_claras['Suma_2017']=union_claras['%2017']
-union_claras['Suma_2018']=union_claras['%2018']
-union_claras['Suma_2019']=union_claras['%2019']
-union_claras['Suma_2020']=union_claras['%2020']
-union_claras['Suma_2021']=union_claras['%2021']
-union_claras['Suma_2022']=union_claras['%2022']
-ac_claras=union_claras
-ac_claras['Fecha_apro']=ac_claras['Fecha_apro'].astype(float)
+#Second part: Join CSV and vectorial layer through ID column
+#Thinning
+join_T=pd.merge(dC, SHP_T, on='ID')
+join_T['Fecha_apro']=join_T['Fecha_apro'].astype(float)
 
 #CH
-
-
-union_cortas=pd.merge(dCH, SHP_Cortas, on='ID')
-
-
-union_cortas['%2016']=(union_cortas['P1_16']/(union_cortas['N_pixel']))*100
-union_cortas['%2017']=(union_cortas['P1_17']/(union_cortas['N_pixel']))*100
-union_cortas['%2018']=(union_cortas['P1_18']/(union_cortas['N_pixel']))*100
-union_cortas['%2019']=(union_cortas['P1_19']/(union_cortas['N_pixel']))*100
-union_cortas['%2020']=((union_cortas['P1_20'])/(union_cortas['N_pixel']))*100
-union_cortas['%2021']=((union_cortas['P1_21'])/(union_cortas['N_pixel']))*100
-union_cortas['%2022']=(union_cortas['P1_22'])/(union_cortas['N_pixel'])*100
-
-union_cortas['Suma_2016']=union_cortas['%2016']
-union_cortas['Suma_2017']=union_cortas['%2017']
-union_cortas['Suma_2018']=union_cortas['%2018']
-union_cortas['Suma_2019']=union_cortas['%2019']
-union_cortas['Suma_2020']=union_cortas['%2020']
-union_cortas['Suma_2021']=union_cortas['%2021']
-union_cortas['Suma_2022']=union_cortas['%2022']
-
-ac_cortas=union_cortas
-
-ac_cortas['Fecha_apro']=ac_cortas['Fecha_apro'].astype(float)
-
+join_CC=pd.merge(dCH, SHP_CC, on='ID')
+join_CC['Fecha_apro']=join_CC['Fecha_apro'].astype(float)
 
 #Control
 
 SHP_Control.columns = ['ID', 'Especie', 'Area', 'AREA_Calcu', 'Perimeter', 'RATIO', 'Numero','ABREV','geometry']
+join_control=pd.merge(dCon, SHP_Control, on='ID')
 
-union_control=pd.merge(dCon, SHP_Control, on='ID')
-
-
-union_control['%2016']=(union_control['P1_16']/(union_control['N_pixel']))*100
-union_control['%2017']=(union_control['P1_17']/(union_control['N_pixel']))*100
-union_control['%2018']=(union_control['P1_18']/(union_control['N_pixel']))*100
-union_control['%2019']=(union_control['P1_19']/(union_control['N_pixel']))*100
-union_control['%2020']=((union_control['P1_20'])/(union_control['N_pixel']))*100
-union_control['%2021']=((union_control['P1_21'])/(union_control['N_pixel']))*100
-union_control['%2022']=(union_control['P1_22'])/(union_control['N_pixel'])*100
-
-union_control['Suma_2016']=union_control['%2016']
-union_control['Suma_2017']=union_control['%2017']
-union_control['Suma_2018']=union_control['%2018']
-union_control['Suma_2019']=union_control['%2019']
-union_control['Suma_2020']=union_control['%2020']
-union_control['Suma_2021']=union_control['%2021']
-union_control['Suma_2022']=union_control['%2022']
-union_control.columns
-ac_control=union_control
-
-
-aprov = pd.concat([cortas,claras, control])
-
+#Third part: join all types of plots
+aprov = pd.concat([join_CC,join_T,join_control])
 aprov['Clara_Hech']=aprov['Clara_Hech'].fillna('N')
 
 
+# Fourth part: Classify all plots with the optimal threshold
 
-#Ver
 a=0.022
 b=0.001
 
-
+#This code is for SWIR2, if you want to evaluate SWIR1 change M7 for M6
 aprov.loc[(aprov['M7_16']>=b)&(aprov['M7_16']<a), ['2016']] = ['C']
 aprov.loc[(aprov['M7_16']<b), ['2016']] = ['N']
 aprov.loc[(aprov['M7_16']>a), ['2016']] = ['H']
-
-
 aprov.loc[(aprov['M7_17']>=b)&(aprov['M7_17']<a), ['2017']] = ['C']
 aprov.loc[(aprov['M7_17']<b), ['2017']] = ['N']
 aprov.loc[(aprov['M7_17']>a), ['2017']] = ['H']
-
 aprov.loc[(aprov['M7_18']>=b)&(aprov['M7_18']<a), ['2018']] = ['C']
 aprov.loc[(aprov['M7_18']<b), ['2018']] = ['N']
 aprov.loc[(aprov['M7_18']>a), ['2018']] = ['H']
-
 aprov.loc[(aprov['M7_19']>=b)&(aprov['M7_19']<a), ['2019']] = ['C']
 aprov.loc[(aprov['M7_19']<b), ['2019']] = ['N']
 aprov.loc[(aprov['M7_19']>a), ['2019']] = ['H']
-
 aprov.loc[(aprov['M7_20']>=b)&(aprov['M7_20']<a), ['2020']] = ['C']
 aprov.loc[(aprov['M7_20']<b), ['2020']] = ['N']
 aprov.loc[(aprov['M7_20']>a), ['2020']] = ['H']
-
-
 aprov.loc[(aprov['M7_21']>=b)&(aprov['M7_21']<a), ['2021']] = ['C']
 aprov.loc[(aprov['M7_21']<b), ['2021']] = ['N']
 aprov.loc[(aprov['M7_21']>a), ['2021']] = ['H']
-
 aprov.loc[(aprov['M7_22']>=b)&(aprov['M7_22']<a), ['2022']] = ['C']
 aprov.loc[(aprov['M7_22']<b), ['2022']] = ['N']
 aprov.loc[(aprov['M7_22']>a), ['2022']] = ['H']
-
 aprov['2016']=aprov['2016'].fillna('N')
 aprov['2017']=aprov['2017'].fillna('N')
 aprov['2018']=aprov['2018'].fillna('N')
@@ -177,20 +105,11 @@ for y in range(len(year)):
         else:
             break
 
-
-#Si en todos los años hay control
-
 aprov.loc[(aprov['Clara_Hech']=='N')&(aprov['2018'] =='N')&(aprov['2019'] =='N')&(aprov['2020'] =='N')&(aprov['2021'] =='N'), ['verif']] = ['T']
-
 aprov['pred']='X'
-
 aprov.loc[(aprov['verif'] =='T')&(aprov['Clara_Hech'] =='H'), ['pred']] = ['H']
 aprov.loc[(aprov['verif'] =='T')&(aprov['Clara_Hech'] =='C'), ['pred']] = ['C']
 aprov.loc[(aprov['verif'] =='T')&(aprov['Clara_Hech'] =='N'), ['pred']] = ['N']
-
-
-
-#Falsos coloca la clasificacion que tiene esa fecha
 year=['2016','2017','2018','2019','2020','2021','2022','2023']
 for i in range(len(year)):
     if i<5:
@@ -212,18 +131,16 @@ for i in range(len(year)):
     else:
         break
 
-
-#Falsos de control coloca la primera ruptura
 year=['2018','2019','2020','2021']
 for h in range(len(year)):
         aprov.loc[(aprov['verif'] =='F')&(aprov['Clara_Hech']=='N')&(aprov[year[h]]=='C')&(aprov['pred']=='X'),['pred']] = ['C']
         aprov.loc[(aprov['verif'] =='F')&(aprov['Clara_Hech']=='N')&(aprov[year[h]]=='H')&(aprov['pred']=='X'),['pred']] = ['H']
-
-
 y_aprov=np.array(aprov['pred'])
 y_true_aprov=np.array(aprov['Clara_Hech'])
 
+#Calculate a confusion matrix in order to see the result
 confusion_matrix(y_true_aprov, y_aprov, labels=['C','H','N'])
 
+#Fifth part: Export the result in a CSV
 clas_B7=gpd.GeoDataFrame(aprov, geometry='geometry')
-clas_B7.to_csv('Clasificacion_B7&B6_50nub.csv', header=True, index=True)
+clas_B7.to_csv('Clasification_B7&B6.csv', header=True, index=True)
