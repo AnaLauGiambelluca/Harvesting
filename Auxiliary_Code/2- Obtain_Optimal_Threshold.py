8957da4dd0db0c2bc4bddc237109c17f6a5dfb54
AnaLauGiambelluca <148961897+AnaLauGiambelluca@users.noreply.github.com>
2024-01-09 11:07:25 +0100

    Update 2- Obtain_Optimal_Threshold.py
    Code to obtain optimal thresholds using magnitude as variable

--- Auxiliary_Code/2- Obtain_Optimal_Threshold.py
+++ Auxiliary_Code/2- Obtain_Optimal_Threshold.py
@@ -1,4 +1,6 @@
-#Importe de librerias
+# Code to obtain the optimal threshold is you used magnitude of SWIR2 or SWIR1 as variable
+
+#Import libraries
 
 import pandas as pd
 import numpy as np
@@ -11,201 +13,120 @@ import rasterio.warp
 from osgeo import gdal
 from matplotlib import pyplot
 from rasterio.plot import show 
-
 import geopandas as gpd
-#Empezamos cargando librerías
 from osgeo import ogr, osr
-
 from sklearn.metrics import confusion_matrix
 from sklearn.metrics import ConfusionMatrixDisplay, classification_report,f1_score
 from sklearn.metrics import accuracy_score
-
 from sklearn.model_selection import train_test_split 
 
+# First part: upload CSV and vectorial layers
 
-os.chdir(r'C:\Users\giambelluca.144055\OneDrive\Doctorado\Datos_Laura')
+direct='Path'
+os.chdir(direct)
 
-dC = pd.read_csv('Claras_B7_B6_Prob_MAG.csv')
-dCon = pd.read_csv('Control_B7_B6_Prob_MAG.csv')
-dCH = pd.read_csv('Cortas_B7_B6_Prob_MAG.csv')
+dC = pd.read_csv('Thinning_B7_B6.csv')
+dCon = pd.read_csv('Control_B7_B6.csv')
+dCH = pd.read_csv('CC_B7_B6.csv')
 
+direct_vect='Path'
+os.chdir(direct_vect)
+SHP_T=gpd.read_file('Thinning_plot.shp')
+SHP_Control=gpd.read_file('Control_plot.shp')
+SHP_CC=gpd.read_file('Clear_cutting_plot.shp')
 
-os.chdir(r'C:\Users\giambelluca.144055\OneDrive\Doctorado\Capas\Purgadas')
-SHP_Claras=gpd.read_file('Claras_10102023.shp')
-SHP_Control=gpd.read_file('Control_10102023.shp')
-SHP_Cortas=gpd.read_file('Cortas_10102023.shp')
+# Second part: join vectorial layer with CSV information through ID column.
+#Thinning
 
-#Claras
+join_T=pd.merge(dC, SHP_T, on='ID')
+join_T['Fecha_apro']=join_T['Fecha_apro'].astype(float)
 
-union_claras=pd.merge(dC, SHP_Claras, on='ID')
+#Clear-cutting
 
-union_claras['%2016']=(union_claras['P1_16']/(union_claras['N_pixel']))*100
-union_claras['%2017']=(union_claras['P1_17']/(union_claras['N_pixel']))*100
-union_claras['%2018']=(union_claras['P1_18']/(union_claras['N_pixel']))*100
-union_claras['%2019']=(+union_claras['P1_19']/(union_claras['N_pixel']))*100
-union_claras['%2020']=((union_claras['P1_20'])/(union_claras['N_pixel']))*100
-union_claras['%2021']=((union_claras['P1_21'])/(union_claras['N_pixel']))*100
-union_claras['%2022']=(union_claras['P1_22'])/(union_claras['N_pixel'])*100
-
-union_claras['Suma_2016']=union_claras['%2016']
-union_claras['Suma_2017']=union_claras['%2017']
-union_claras['Suma_2018']=union_claras['%2018']
-union_claras['Suma_2019']=union_claras['%2019']
-union_claras['Suma_2020']=union_claras['%2020']
-union_claras['Suma_2021']=union_claras['%2021']
-union_claras['Suma_2022']=union_claras['%2022']
-ac_claras=union_claras
-ac_claras['Fecha_apro']=ac_claras['Fecha_apro'].astype(float)
-
-#CH
-
-
-union_cortas=pd.merge(dCH, SHP_Cortas, on='ID')
-
-
-union_cortas['%2016']=(union_cortas['P1_16']/(union_cortas['N_pixel']))*100
-union_cortas['%2017']=(union_cortas['P1_17']/(union_cortas['N_pixel']))*100
-union_cortas['%2018']=(union_cortas['P1_18']/(union_cortas['N_pixel']))*100
-union_cortas['%2019']=(union_cortas['P1_19']/(union_cortas['N_pixel']))*100
-union_cortas['%2020']=((union_cortas['P1_20'])/(union_cortas['N_pixel']))*100
-union_cortas['%2021']=((union_cortas['P1_21'])/(union_cortas['N_pixel']))*100
-union_cortas['%2022']=(union_cortas['P1_22'])/(union_cortas['N_pixel'])*100
-
-union_cortas['Suma_2016']=union_cortas['%2016']
-union_cortas['Suma_2017']=union_cortas['%2017']
-union_cortas['Suma_2018']=union_cortas['%2018']
-union_cortas['Suma_2019']=union_cortas['%2019']
-union_cortas['Suma_2020']=union_cortas['%2020']
-union_cortas['Suma_2021']=union_cortas['%2021']
-union_cortas['Suma_2022']=union_cortas['%2022']
-
-ac_cortas=union_cortas
-
-ac_cortas['Fecha_apro']=ac_cortas['Fecha_apro'].astype(float)
-
+join_CC=pd.merge(dCH, SHP_CC, on='ID')
+join_CC['Fecha_apro']=join_CC['Fecha_apro'].astype(float)
 
 #Control
 
 SHP_Control.columns = ['ID', 'Especie', 'Area', 'Area_Calcu', 'Perimeter', 'RATIO', 'Numero','ABREV','geometry']
+join_control=pd.merge(dCon, SHP_Control, on='ID')
 
-union_control=pd.merge(dCon, SHP_Control, on='ID')
+# Third part: Select randomly 220 plot for each forest harvesting practice
+sel_CC=join_CC.sample(n = 220, random_state = 4)
+sel_T=join_T.sample(n = 220, random_state = 4)
+sel_control=join_control.sample(n = 220, random_state = 4)
 
-
-union_control['%2016']=(union_control['P1_16']/(union_control['N_pixel']))*100
-union_control['%2017']=(union_control['P1_17']/(union_control['N_pixel']))*100
-union_control['%2018']=(union_control['P1_18']/(union_control['N_pixel']))*100
-union_control['%2019']=(union_control['P1_19']/(union_control['N_pixel']))*100
-union_control['%2020']=((union_control['P1_20'])/(union_control['N_pixel']))*100
-union_control['%2021']=((union_control['P1_21'])/(union_control['N_pixel']))*100
-union_control['%2022']=(union_control['P1_22'])/(union_control['N_pixel'])*100
-
-union_control['Suma_2016']=union_control['%2016']
-union_control['Suma_2017']=union_control['%2017']
-union_control['Suma_2018']=union_control['%2018']
-union_control['Suma_2019']=union_control['%2019']
-union_control['Suma_2020']=union_control['%2020']
-union_control['Suma_2021']=union_control['%2021']
-union_control['Suma_2022']=union_control['%2022']
-union_control.columns
-ac_control=union_control
-
-
-cortas=pd.concat([selec_cortas,selec_CH])
-claras=pd.concat([selec_C,selec_claras])
-control=ac_control
-cortas.loc[(cortas['ID'] == 268)|(cortas['ID'] == 269), ['Clara_Hech']] = ['H']
-
-sel_cortas=cortas.sample(n = 220, random_state = 4)
-sel_claras=claras.sample(n = 220, random_state = 4)
-sel_control=control.sample(n = 220, random_state = 4)
-
+# Fourth part: Split samples in train (70%) and test (30%)
 np.random.seed(1)
-
-random_claras, random_claras_test = train_test_split(sel_claras, test_size = 0.30)
-
+random_T, random_T_test = train_test_split(sel_T, test_size = 0.30)
 np.random.seed(1)
-
-random_cortas, random_cortas_test = train_test_split(sel_cortas, test_size = 0.30)
-
+random_CC, random_CC_test = train_test_split(sel_CC, test_size = 0.30)
 np.random.seed(1)
-
 random_control, random_control_test = train_test_split(sel_control, test_size = 0.30)
 
-unido = pd.concat([random_claras,random_cortas, random_control])
-# unido = pd.concat([cortas,claras, control])
-unido['Clara_Hech']=unido['Clara_Hech'].fillna('N')
+#Join the train plots
+aprov = pd.concat([random_T,random_CC, random_control])
+aprov['Clara_Hech']=aprov['Clara_Hech'].fillna('N')
 
-umbral_bajo=[]
-umbral_alto=[]
-mag_baja=[]
-mag_alta=[]
+#Fifth part: loop to know the optimal threshold
+
+mag_low=[]
+mag_high=[]
 f_score_micro=[]
 f_score_macro=[]
 accuracy=[]
 
+# Depend on the band considered the threshold might change. In this case is for SWIR2 or SWIR1
 for c in range (1000,-1,-10):
         for d in range (10,1001,10):
             a=c/10000
             b=d/10000
             if a>b:
-#                     aprov.loc[(aprov['M7_16']>=b)&(aprov['M7_16']<a), ['2016']] = ['C']
-#                     aprov.loc[(aprov['M7_16']<b), ['2016']] = ['N']
-#                     aprov.loc[(aprov['M7_16']>a), ['2016']] = ['H']
+                   # In this example SWIR2 magnitude will be tested, for SWIR1 comment the following code and uncomment the next.
+                    aprov.loc[(aprov['M7_16']>=b)&(aprov['M7_16']<a), ['2016']] = ['C']
+                    aprov.loc[(aprov['M7_16']<b), ['2016']] = ['N']
+                    aprov.loc[(aprov['M7_16']>a), ['2016']] = ['H']
+                    aprov.loc[(aprov['M7_17']>=b)&(aprov['M7_17']<a), ['2017']] = ['C']
+                    aprov.loc[(aprov['M7_17']<b), ['2017']] = ['N']
+                    aprov.loc[(aprov['M7_17']>a), ['2017']] = ['H']
+                    aprov.loc[(aprov['M7_18']>=b)&(aprov['M7_18']<a), ['2018']] = ['C']
+                    aprov.loc[(aprov['M7_18']<b), ['2018']] = ['N']
+                    aprov.loc[(aprov['M7_18']>a), ['2018']] = ['H']
+                    aprov.loc[(aprov['M7_19']>=b)&(aprov['M7_19']<a), ['2019']] = ['C']
+                    aprov.loc[(aprov['M7_19']<b), ['2019']] = ['N']
+                    aprov.loc[(aprov['M7_19']>a), ['2019']] = ['H']
+                    aprov.loc[(aprov['M7_20']>=b)&(aprov['M7_20']<a), ['2020']] = ['C']
+                    aprov.loc[(aprov['M7_20']<b), ['2020']] = ['N']
+                    aprov.loc[(aprov['M7_20']>a), ['2020']] = ['H']
+                    aprov.loc[(aprov['M7_21']>=b)&(aprov['M7_21']<a), ['2021']] = ['C']
+                    aprov.loc[(aprov['M7_21']<b), ['2021']] = ['N']
+                    aprov.loc[(aprov['M7_21']>a), ['2021']] = ['H']
+                    aprov.loc[(aprov['M7_22']>=b)&(aprov['M7_22']<a), ['2022']] = ['C']
+                    aprov.loc[(aprov['M7_22']<b), ['2022']] = ['N']
+                    aprov.loc[(aprov['M7_22']>a), ['2022']] = ['H']
 
-
-#                     aprov.loc[(aprov['M7_17']>=b)&(aprov['M7_17']<a), ['2017']] = ['C']
-#                     aprov.loc[(aprov['M7_17']<b), ['2017']] = ['N']
-#                     aprov.loc[(aprov['M7_17']>a), ['2017']] = ['H']
-
-#                     aprov.loc[(aprov['M7_18']>=b)&(aprov['M7_18']<a), ['2018']] = ['C']
-#                     aprov.loc[(aprov['M7_18']<b), ['2018']] = ['N']
-#                     aprov.loc[(aprov['M7_18']>a), ['2018']] = ['H']
-
-#                     aprov.loc[(aprov['M7_19']>=b)&(aprov['M7_19']<a), ['2019']] = ['C']
-#                     aprov.loc[(aprov['M7_19']<b), ['2019']] = ['N']
-#                     aprov.loc[(aprov['M7_19']>a), ['2019']] = ['H']
-
-#                     aprov.loc[(aprov['M7_20']>=b)&(aprov['M7_20']<a), ['2020']] = ['C']
-#                     aprov.loc[(aprov['M7_20']<b), ['2020']] = ['N']
-#                     aprov.loc[(aprov['M7_20']>a), ['2020']] = ['H']
-
-
-#                     aprov.loc[(aprov['M7_21']>=b)&(aprov['M7_21']<a), ['2021']] = ['C']
-#                     aprov.loc[(aprov['M7_21']<b), ['2021']] = ['N']
-#                     aprov.loc[(aprov['M7_21']>a), ['2021']] = ['H']
-
-#                     aprov.loc[(aprov['M7_22']>=b)&(aprov['M7_22']<a), ['2022']] = ['C']
-#                     aprov.loc[(aprov['M7_22']<b), ['2022']] = ['N']
-#                     aprov.loc[(aprov['M7_22']>a), ['2022']] = ['H']
-                    aprov.loc[(aprov['Mb6_16']>=b)&(aprov['Mb6_16']<a), ['2016']] = ['C']
-                    aprov.loc[(aprov['Mb6_16']<b), ['2016']] = ['N']
-                    aprov.loc[(aprov['Mb6_16']>a), ['2016']] = ['H']
-
-
-                    aprov.loc[(aprov['Mb6_17']>=b)&(aprov['Mb6_17']<a), ['2017']] = ['C']
-                    aprov.loc[(aprov['Mb6_17']<b), ['2017']] = ['N']
-                    aprov.loc[(aprov['Mb6_17']>a), ['2017']] = ['H']
-
-                    aprov.loc[(aprov['Mb6_18']>=b)&(aprov['Mb6_18']<a), ['2018']] = ['C']
-                    aprov.loc[(aprov['Mb6_18']<b), ['2018']] = ['N']
-                    aprov.loc[(aprov['Mb6_18']>a), ['2018']] = ['H']
-
-                    aprov.loc[(aprov['Mb6_19']>=b)&(aprov['Mb6_19']<a), ['2019']] = ['C']
-                    aprov.loc[(aprov['Mb6_19']<b), ['2019']] = ['N']
-                    aprov.loc[(aprov['Mb6_19']>a), ['2019']] = ['H']
-
-                    aprov.loc[(aprov['Mb6_20']>=b)&(aprov['Mb6_20']<a), ['2020']] = ['C']
-                    aprov.loc[(aprov['Mb6_20']<b), ['2020']] = ['N']
-                    aprov.loc[(aprov['Mb6_20']>a), ['2020']] = ['H']
-
-
-                    aprov.loc[(aprov['Mb6_21']>=b)&(aprov['Mb6_21']<a), ['2021']] = ['C']
-                    aprov.loc[(aprov['Mb6_21']<b), ['2021']] = ['N']
-                    aprov.loc[(aprov['Mb6_21']>a), ['2021']] = ['H']
-
-                    aprov.loc[(aprov['Mb6_22']>=b)&(aprov['Mb6_22']<a), ['2022']] = ['C']
-                    aprov.loc[(aprov['Mb6_22']<b), ['2022']] = ['N']
-                    aprov.loc[(aprov['Mb6_22']>a), ['2022']] = ['H']
+                    #SWIR1
+                    # aprov.loc[(aprov['Mb6_16']>=b)&(aprov['Mb6_16']<a), ['2016']] = ['C']
+                    # aprov.loc[(aprov['Mb6_16']<b), ['2016']] = ['N']
+                    # aprov.loc[(aprov['Mb6_16']>a), ['2016']] = ['H']
+                    # aprov.loc[(aprov['Mb6_17']>=b)&(aprov['Mb6_17']<a), ['2017']] = ['C']
+                    # aprov.loc[(aprov['Mb6_17']<b), ['2017']] = ['N']
+                    # aprov.loc[(aprov['Mb6_17']>a), ['2017']] = ['H']
+                    # aprov.loc[(aprov['Mb6_18']>=b)&(aprov['Mb6_18']<a), ['2018']] = ['C']
+                    # aprov.loc[(aprov['Mb6_18']<b), ['2018']] = ['N']
+                    # aprov.loc[(aprov['Mb6_18']>a), ['2018']] = ['H']
+                    # aprov.loc[(aprov['Mb6_19']>=b)&(aprov['Mb6_19']<a), ['2019']] = ['C']
+                    # aprov.loc[(aprov['Mb6_19']<b), ['2019']] = ['N']
+                    # aprov.loc[(aprov['Mb6_19']>a), ['2019']] = ['H']
+                    # aprov.loc[(aprov['Mb6_20']>=b)&(aprov['Mb6_20']<a), ['2020']] = ['C']
+                    # aprov.loc[(aprov['Mb6_20']<b), ['2020']] = ['N']
+                    # aprov.loc[(aprov['Mb6_20']>a), ['2020']] = ['H']
+                    # aprov.loc[(aprov['Mb6_21']>=b)&(aprov['Mb6_21']<a), ['2021']] = ['C']
+                    # aprov.loc[(aprov['Mb6_21']<b), ['2021']] = ['N']
+                    # aprov.loc[(aprov['Mb6_21']>a), ['2021']] = ['H']
+                    # aprov.loc[(aprov['Mb6_22']>=b)&(aprov['Mb6_22']<a), ['2022']] = ['C']
+                    # aprov.loc[(aprov['Mb6_22']<b), ['2022']] = ['N']
+                    # aprov.loc[(aprov['Mb6_22']>a), ['2022']] = ['H']
 
                     aprov['verif']='F'
                     aprov['2023']='X'
@@ -222,22 +143,14 @@ for c in range (1000,-1,-10):
                             aprov.loc[(aprov['Fecha_apro'] ==int(year[y+1]))&(aprov['Clara_Hech'] =='H')&(aprov[year[y]] =='H')&(aprov['verif'] !='T'), ['verif']] = ['T']
                             aprov.loc[(aprov['Fecha_apro'] ==int(year[y+1]))&(aprov['Clara_Hech'] =='H')&(aprov[year[y+2]] =='H')&(aprov['verif'] !='T'), ['verif']] = ['T']
 
-
                         else:
                             break
 
-
-                    #Si en todos los años hay control
-
                     aprov.loc[(aprov['Clara_Hech']=='N')&(aprov['2018'] =='N')&(aprov['2019'] =='N')&(aprov['2020'] =='N')&(aprov['2021'] =='N'), ['verif']] = ['T']
-
                     aprov['pred']='X'
-
                     aprov.loc[(aprov['verif'] =='T')&(aprov['Clara_Hech'] =='H'), ['pred']] = ['H']
                     aprov.loc[(aprov['verif'] =='T')&(aprov['Clara_Hech'] =='C'), ['pred']] = ['C']
                     aprov.loc[(aprov['verif'] =='T')&(aprov['Clara_Hech'] =='N'), ['pred']] = ['N']
-
-
 
                     #Falsos coloca la clasificacion que tiene esa fecha
                     year=['2016','2017','2018','2019','2020','2021','2022','2023']
@@ -261,8 +174,6 @@ for c in range (1000,-1,-10):
                             else:
                                 break
 
-
-                    #Falsos de control coloca la primera ruptura
                     year=['2018','2019','2020','2021']
                     for h in range(len(year)):
                             aprov.loc[(aprov['verif'] =='F')&(aprov['Clara_Hech']=='N')&(aprov[year[h]]=='C')&(aprov['pred']=='X'),['pred']] = ['C']
@@ -276,33 +187,33 @@ for c in range (1000,-1,-10):
                     accuracy_x=accuracy_score(y_true_aprov, y_aprov)
                     f_score_micro.append(f_score_mic)
                     f_score_macro.append(f_score_mac)
-                    mag_alta.append(a)
-                    mag_baja.append(b)
+                    mag_high.append(a)
+                    mag_low.append(b)
                     accuracy.append(accuracy_x)
 
                     print(c,d)
 
 
 analisis= pd.DataFrame()
-analisis['Magnitud_alta']=mag_alta
-analisis['Magnitud_baja']=mag_baja
+analisis['Magnitud_alta']=mag_high
+analisis['Magnitud_baja']=mag_low
 analisis['F-Score_Macro']=f_score_macro
 analisis['F-Score_Micro']=f_score_micro
 analisis['Accuracy']=accuracy
+#Show the highest value of F-SCORE
 analisis.loc[(analisis['F-Score_Macro']==analisis['F-Score_Macro'].max())]
 
+# Sixth part: join test plots
 
-unido = pd.concat([random_claras_test,random_cortas_test, random_control_test])
-unido['Clara_Hech']=unido['Clara_Hech'].fillna('N')
-aprov=unido
+aprov = pd.concat([random_T_test,random_CC_test, random_control_test])
+aprov['Clara_Hech']=aprov['Clara_Hech'].fillna('N')
 
-
-#Ver
+# Seventh part: Verification
+#Put optimal thresholds
 a=0.034
 b=0.001
-x=3
-i=30
 
+#If you want to verified SWIR1 change M7 for M6
 aprov.loc[(aprov['M7_16']>=b)&(aprov['M7_16']<a), ['2016']] = ['C']
 aprov.loc[(aprov['M7_16']<b), ['2016']] = ['N']
 aprov.loc[(aprov['M7_16']>a), ['2016']] = ['H']
@@ -358,20 +269,13 @@ for y in range(len(year)):
         else:
             break
 
-
-#Si en todos los años hay control
-
 aprov.loc[(aprov['Clara_Hech']=='N')&(aprov['2018'] =='N')&(aprov['2019'] =='N')&(aprov['2020'] =='N')&(aprov['2021'] =='N'), ['verif']] = ['T']
-
 aprov['pred']='X'
-
 aprov.loc[(aprov['verif'] =='T')&(aprov['Clara_Hech'] =='H'), ['pred']] = ['H']
 aprov.loc[(aprov['verif'] =='T')&(aprov['Clara_Hech'] =='C'), ['pred']] = ['C']
 aprov.loc[(aprov['verif'] =='T')&(aprov['Clara_Hech'] =='N'), ['pred']] = ['N']
 
 
-
-#Falsos coloca la clasificacion que tiene esa fecha
 year=['2016','2017','2018','2019','2020','2021','2022','2023']
 for i in range(len(year)):
     if i<5:
@@ -393,15 +297,12 @@ for i in range(len(year)):
     else:
         break
 
-
-#Falsos de control coloca la primera ruptura
 year=['2018','2019','2020','2021']
 for h in range(len(year)):
         aprov.loc[(aprov['verif'] =='F')&(aprov['Clara_Hech']=='N')&(aprov[year[h]]=='C')&(aprov['pred']=='X'),['pred']] = ['C']
         aprov.loc[(aprov['verif'] =='F')&(aprov['Clara_Hech']=='N')&(aprov[year[h]]=='H')&(aprov['pred']=='X'),['pred']] = ['H']
-
-
 y_aprov=np.array(aprov['pred'])
 y_true_aprov=np.array(aprov['Clara_Hech'])
 
+# Check the result with a confusion matrix
 confusion_matrix(y_true_aprov, y_aprov, labels=['C','H','N'])
